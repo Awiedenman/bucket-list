992adf78f7aeab7f02d2f64f9e98e92beb3fb8c5
Austin.Wiedenman <awiedenman@gmail.com>
2018-09-04 11:48:08 -0600

    Fetch from db and append on page load

--- public/script.js
+++ public/script.js
@@ -1,6 +1,21 @@
+const listItemRequest = async() => {
+  const url = '/api/v1/bucketList'
+  const response = await fetch(url)
+  const listItems = await response.json();
+  return listItems;
+}
+
+const displayItemsOnLoad = listItems => {
+  listItems.forEach(item => {
+    $('.list').append(`
+      <h2>title: ${item.title} </h2>
+      <p>description: ${item.description} </p>
+      <button class="card--delete"> Delete </button>
+  `)
+  })
+}
 
 const appendListItem = event => {
-  console.log('poop')
   event.preventDefault();
   let titleData = $('.input--title');
   let descriptionData = $('.input--description');
@@ -14,3 +29,10 @@ const appendListItem = event => {
 }
 
 $('.form--button').on('click', appendListItem);
+
+const init = async() => {
+  const initialListItems = await listItemRequest();
+  displayItemsOnLoad(initialListItems);
+}
+
+init();
