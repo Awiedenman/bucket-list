4ece04b4f129364172cd6aabc52b303e5151833e
Austin.Wiedenman <awiedenman@gmail.com>
2018-09-04 10:10:16 -0600

    Build out schema for bucket_list db

--- db/migrations/20180904095705_intitial.js
+++ db/migrations/20180904095705_intitial.js
@@ -1,8 +1,16 @@
 
-exports.up = function(knex, Promise) {
-  
+exports.up = function (knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('bucket_list', function (table) {
+      table.increments('id').primary();
+      table.string('title');
+      table.string('description');
+    })
+  ])
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+  Promise.all([
+    knex.schema.dropTable('bucket_list')
+  ])
 };
